3842d92e4f3c7e3fe02b46128b7378e9d124bf57
Mikolaj Grzaslewicz <mikolaj.grzaslewicz@gmail.com>
2023-06-19 16:37:47 +0200

    Add balance logger
    For manual e2e with real exchange

---
+++ src/main/kotlin/autocoin/binance/bot/BalanceLogger.kt
@@ -0,0 +1,48 @@
+package autocoin.binance.bot
+
+import autocoin.binance.bot.app.config.AppContext
+import autocoin.binance.bot.app.config.loadConfig
+import autocoin.binance.bot.exchange.apikey.ApiKeyId
+import autocoin.binance.bot.exchange.apikey.md5
+import com.autocoin.exchangegateway.api.exchange.apikey.ApiKey
+import com.autocoin.exchangegateway.api.exchange.apikey.ApiKeySupplier
+import com.autocoin.exchangegateway.api.exchange.xchange.ExchangeNames.Companion.binance
+import mu.KotlinLogging
+import java.lang.System.getenv
+import kotlin.system.measureTimeMillis
+
+
+private val logger = KotlinLogging.logger { }
+
+fun main(args: Array<String>) {
+    val durationMillis = measureTimeMillis {
+        val config = loadConfig()
+        logger.info { "Config: $config" }
+        val appContext = AppContext(config)
+        val publicKey = getenv("PUBLIC_KEY")
+        val secretKey = getenv("SECRET_KEY")
+        val currency = getenv("CURRENCY")
+        val apiKey =
+            ApiKeySupplier(
+                id = ApiKeyId(
+                    userId = "cmduser",
+                    keyHash = publicKey.md5() + ":" + secretKey.md5(),
+                ),
+                supplier = {
+                    ApiKey(
+                        publicKey = publicKey,
+                        secretKey = secretKey,
+                    )
+                }
+            )
+        with(appContext) {
+            val balance = walletServiceGateway.getCurrencyBalance(
+                exchangeName = binance,
+                apiKey = apiKey,
+                currencyCode = currency,
+            )
+            logger.info { "Balance: $balance" }
+        }
+    }
+    logger.info { "Finished in ${durationMillis}ms" }
+}
